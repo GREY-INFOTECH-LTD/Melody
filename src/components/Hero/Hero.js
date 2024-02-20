b849b1505512afbf18cbfee9125541476f74eebd
gpaul988 <gpaul988gmail.com>
2024-02-21 00:43:51 +0100

    complete

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="px-4 py-3 my-3 text-center">
     <div className="img-container">
-        <i className="fas fa-headphones-simple fa-8x pb-2 my-2 my-md-4" id="hero-icon"></i>
+        <i className="fa-solid fa-music fa-8x pb-2 my-2 my-md-4" id="hero-icon"></i>
     </div>
       <h1 className="hero-text my-2">Melody</h1>
     </div>
